0e85ac9023bc3c0494a46740509f702d55f1aff2
gonchigars <gonchigars@gmail.com>
2024-07-03 10:29:09 +0530

    asldfjlajsdfd;l

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         VotingMachine myMachine = new VotingMachine();
         myMachine.runVoting();
+        //
 
     }
 }
